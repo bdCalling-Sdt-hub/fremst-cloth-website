1df69f8d1216da8d9ba776e60b2d45009ea392a6
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-04-01 12:47:36 +0600

    updated

--- src/components/ui/website/home/Banner.tsx
+++ src/components/ui/website/home/Banner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Big_Shoulders_Display } from "next/font/google";
-import { Plus_Jakarta_Sans } from "next/font/google";
+// import { Plus_Jakarta_Sans } from "next/font/google";
 import Link from "next/link";
 
 const bigShoulders = Big_Shoulders_Display({
@@ -8,17 +8,17 @@ const bigShoulders = Big_Shoulders_Display({
   weight: ["400", "700"],
 });
 
-const plusJakarta = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
+// const plusJakarta = Plus_Jakarta_Sans({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700"],
+// });
 
 const Banner = () => {
   return (
     <div
       className="w-full md:h-[800px] h-[300px] flex items-center justify-start "
       style={{
-        backgroundImage: `url('/banner.svg')`,
+        backgroundImage: `url('/Handske_header (1).jpg')`,
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
@@ -29,19 +29,19 @@ const Banner = () => {
         <p
           className={`${bigShoulders.className}  w-full uppercase text-white font-[800] text-[32px] md:text-7xl mb-5`}
         >
-          Gloves That Work <br /> as Hard as You Do
+          Welcome to Fremst - <br /> Your one stop shop
         </p>
 
-        <p
+        {/* <p
           className={`${plusJakarta.className}  text-white font-[500] text-[18px] tracking-wider`}
         >
           Select from a variety of models for every purpose.{" "}
-        </p>
+        </p> */}
 
         <Link href={"/products"}>
           <button className=" bg-[#FFC700] w-[220px] font-semibold rounded-full py-2 text-gray-700 text-[16px] mt-3">
             {" "}
-            Explore our gloves
+            Explore our SHOP
           </button>
         </Link>
       </div>
